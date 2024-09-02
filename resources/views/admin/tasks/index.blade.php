420a1625c0d7653144d148854476b937c1333051
modestasv <info@modestasv.lt>
2024-09-02 14:24:49 +0300

    Adding Views

---
+++ resources/views/admin/tasks/index.blade.php
@@ -0,0 +1,51 @@
+<x-layouts.admin>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('All Tasks') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    <div class="overflow-hidden overflow-x-auto p-6 bg-white border-b border-gray-200">
+                        <div class="min-w-full align-middle">
+                            <table class="min-w-full divide-y divide-gray-200 border">
+                                <thead>
+                                <tr>
+                                    <th class="px-6 py-3 bg-gray-50 text-left">
+                                        <span class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Name</span>
+                                    </th>
+                                    <th class="px-6 py-3 bg-gray-50 text-left">
+                                        <span class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">User</span>
+                                    </th>
+                                    <th class="px-6 py-3 bg-gray-50 text-left">
+                                        <span class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Due Date</span>
+                                    </th>
+                                </tr>
+                                </thead>
+
+                                <tbody class="bg-white divide-y divide-gray-200 divide-solid">
+                                @foreach($tasks as $task)
+                                    <tr class="bg-white">
+                                        <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">
+                                            {{ $task->name }}
+                                        </td>
+                                        <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">
+                                            {{ $task->user->name }}
+                                        </td>
+                                        <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">
+                                            {{ $task->due_date }}
+                                        </td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-layouts.admin>
